2e9729d9360ff43f450660c30a2088ca2ca75863
dkbnull <dkbnull@163.com>
2020-01-26 10:30:20 +0800

    代码优化

--- spring-boot-log/src/main/java/cn/wbnull/springbootdemo/service/LogService.java
+++ spring-boot-log/src/main/java/cn/wbnull/springbootdemo/service/LogService.java
@@ -31,24 +31,15 @@ public class LogService {
         response.addHeader("Content-Disposition", "attachment;fileName=" + name);
 
         byte[] buffer = new byte[1024];
-        FileInputStream fis = null;
-        BufferedInputStream bis = null;
-        try {
-            fis = new FileInputStream(file);
-            bis = new BufferedInputStream(fis);
+        try (FileInputStream fis = new FileInputStream(file);
+                      BufferedInputStream bis = new BufferedInputStream(fis)) {
+
             OutputStream os = response.getOutputStream();
 
             int i = bis.read(buffer);
             while (i != -1) {
                 os.write(buffer, 0, i);
                 i = bis.read(buffer);
-            }
-        } finally {
-            if (bis != null) {
-                bis.close();
-            }
-            if (fis != null) {
-                fis.close();
             }
         }
     }
